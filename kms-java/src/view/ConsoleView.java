1944acc9204c82b3f2e2d24052efef42a6a9cdb6
Project Java Fakhri <fakhri.fsl04@gmail.com>
2026-07-03 14:12:58 +0700

    view 2 (fakhri/002)

--- kms-java/src/view/ConsoleView.java
+++ kms-java/src/view/ConsoleView.java
@@ -18,4 +18,31 @@ public class ConsoleView {
         this.input = new InputHandler();
     }
 
+    public void mulai() {
+        boolean lanjut = true;
+        while (lanjut) {
+            tampilkanMenu();
+            int pilihan = input.bacaPilihanMenu("Pilih menu", 1, 7);
+            switch (pilihan) {
+                case 1: prosesTambahData(); break;
+                case 2: prosesLihatSemua(); break;
+                case 3: prosesCariData(); break;
+                case 4: prosesFilterData(); break;
+                case 5: prosesHapusData(); break;
+                case 6: prosesStatistik(); break;
+                case 7: lanjut = false; System.out.println("Sampai jumpa!"); break;
+            }
+        }
+    }
+    private void tampilkanMenu() {
+        System.out.println("\n===== KMS PUTUSAN NARKOTIKA =====");
+        System.out.println("1. Tambah Data");
+        System.out.println("2. Lihat Semua Data");
+        System.out.println("3. Cari Data");
+        System.out.println("4. Filter Data");
+        System.out.println("5. Hapus Data");
+        System.out.println("6. Lihat Statistik");
+        System.out.println("7. Keluar");
+    }
+
 }
