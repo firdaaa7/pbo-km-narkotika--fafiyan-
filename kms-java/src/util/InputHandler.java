8c5fc74fd8d32da0caf189ac433462e41f1f478f
modul 1 <firdafebiyanti528@gmail.com>
2026-07-03 14:09:46 +0700

    commit inpithandler 2 firda015

--- kms-java/src/util/InputHandler.java
+++ kms-java/src/util/InputHandler.java
@@ -3,3 +3,10 @@ package util;
 import java.util.Scanner;
 
 public class InputHandler {
+
+    private Scanner scanner;
+
+    public InputHandler() {
+        this.scanner = new Scanner(System.in);
+    }
+
