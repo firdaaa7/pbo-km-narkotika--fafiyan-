807dc71a7190a4ff98c412760c40c5ca73ec91c5
modul 1 <firdafebiyanti528@gmail.com>
2026-07-03 14:44:31 +0700

    commit kc 2 firda015

--- kms-java/src/controller/KnowledgeController.java
+++ kms-java/src/controller/KnowledgeController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import model.Putusan;
+import model.KnowledgeRepository;
+import model.StatistikPutusan;
+import util.DataTidakValidException;
+import java.util.ArrayList;
+
 public class KnowledgeController {
-}
+
+    private KnowledgeRepository repository;
+
+    public KnowledgeController() {
+        this.repository = new KnowledgeRepository();
+    }
+
