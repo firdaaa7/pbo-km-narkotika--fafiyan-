177fd9df222b30f6f6fff8a9bb15e2b7f78accfb
modul 1 <firdafebiyanti528@gmail.com>
2026-07-03 14:18:08 +0700

    commit inpithandler 3 firda015

--- kms-java/src/util/InputHandler.java
+++ kms-java/src/util/InputHandler.java
@@ -10,3 +10,15 @@ public class InputHandler {
         this.scanner = new Scanner(System.in);
     }
 
+    public String bacaTeks(String label) {
+        String input;
+        do {
+            System.out.print(label + ": ");
+            input = scanner.nextLine().trim();
+            if (input.isEmpty()) {
+                System.out.println("Input tidak boleh kosong, coba lagi.");
+            }
+        } while (input.isEmpty());
+        return input;
+    }
+
